593215122393cdbdd466de1ac1ff77879b9d19df
Ahmad <ahmadvegah@gmail.com>
2021-10-16 18:36:12 +0100

    camera update to gstreamer and added kinect camera

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -132,11 +132,11 @@ class CSICamera(BaseCamera):
     Credit: https://github.com/feicccccccc/donkeycar/blob/dev/donkeycar/parts/camera.py
     gstreamer init string from https://github.com/NVIDIA-AI-IOT/jetbot/blob/master/jetbot/camera.py
     '''
-    def gstreamer_pipeline(self, capture_width=3280, capture_height=2464, output_width=224, output_height=224, framerate=21, flip_method=0) :   
+    def gstreamer_pipeline(self, capture_width=1920, capture_height=1080, output_width=224, output_height=224, framerate=21, flip_method=0) :   
         return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=%d ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
                 capture_width, capture_height, framerate, flip_method, output_width, output_height)
     
-    def __init__(self, image_w=160, image_h=120, image_d=3, capture_width=3280, capture_height=2464, framerate=60, gstreamer_flip=0):
+    def __init__(self, image_w=160, image_h=120, image_d=3, capture_width=1920, capture_height=1080, framerate=60, gstreamer_flip=0):
         '''
         gstreamer_flip = 0 - no flip
         gstreamer_flip = 1 - rotate CCW 90
